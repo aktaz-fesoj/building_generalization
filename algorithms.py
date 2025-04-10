58dae395d064a3ba511c66d30fe089b21f3fb0d1
aktaz-fesoj <josefvicko@gmail.com>
2025-04-10 10:56:17 +0200

    wa

--- algorithms.py
+++ algorithms.py
@@ -352,9 +352,9 @@ class Algorithms:
         #σ = σ₁ + (∑ rᵢ·sᵢ) / (∑ sᵢ) = calculate the main direction of building   
         main_direction = sigma_base + ri_edge_len_sum / edge_len_sum
         
-        building_rotated = self.rotate(building, main_direction)
+        building_rotated = self.rotate(building, -main_direction)
         mmb, area = self.createMMB(building_rotated)
-        mmb_rotated = self.rotate(mmb, -main_direction)
+        mmb_rotated = self.rotate(mmb, main_direction)
         mmb_resized = self.resizeRectangle(building, mmb_rotated)
 
         return mmb_resized
